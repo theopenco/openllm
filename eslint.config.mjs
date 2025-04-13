3d1e8d99a6ae41bc97a57d50b3e285091e54e4de
Luca Steeb <contact@luca-steeb.com>
2025-04-13 16:33:01 +0700

    chore(project): update configs and dependencies
    Updated @steebchen/lint-next to v1.18.2, adjusted ignore rules, added
    API tsconfig, and enhanced eslint/tsconfig options for consistency.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,9 @@
 import lint from "@steebchen/eslint-config";
 
 /** @type {import("eslint").Linter.Config[]} */
-export default [...lint];
+export default [
+	...lint,
+	{
+		ignores: ["**/.vinxi/**"],
+	},
+];
